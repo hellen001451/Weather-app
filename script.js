1bb6c464e25c11f255e5d741560eed65d6319de2
hellen001451 <125268394+hellen001451@users.noreply.github.com>
2024-06-24 06:15:31 +0300

    date setting

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ const apiKey = "a97c5ae048f4155d874a7af8bb6486ef";
         const searchInput = document.querySelector(".search input");
         const searchBtn = document.querySelector(".search button");
         
+        
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+    setInterval(() => {
+        const time = new Date();
+        const month = time.getMonth();
+        const date = time.getDate();
+        const day = time.getDay();
+        const hour = time.getHours();
+        const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+        const minutes = time.getMinutes();
+        const ampm = hour >=12 ? 'PM' : 'AM'
+    
+        document.getElementById("time").innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
+    
+        document.getElementById("date").innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+    
+    }, 1000);
+    
 
         async function checkWeather(city){
             const response= await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -22,36 +41,40 @@ const apiKey = "a97c5ae048f4155d874a7af8bb6486ef";
             document.querySelector(".wind").innerHTML = data.list[0].wind.speed + "km/hr";
             document.querySelector(".humidity").innerHTML = data.list[0].main.humidity + "%";
             document.querySelector(".feels_like").innerHTML = data.list[0].main.feels_like + "°C";
-            document.getElementById("time").innerHTML = data.list[0].dt_txt;
             document.getElementById("description").innerHTML = data.list[0].weather[0].description;
+            
             document.querySelector(".temp1").innerHTML = Math.round(data.list[4].main.temp) + "°C";
             document.querySelector(".temp2").innerHTML = Math.round(data.list[12].main.temp) + "°C";
             document.querySelector(".temp3").innerHTML = Math.round(data.list[22].main.temp) + "°C";
             document.querySelector(".temp4").innerHTML = Math.round(data.list[30].main.temp) + "°C";
             document.querySelector(".temp5").innerHTML = Math.round(data.list[37].main.temp) + "°C";
             document.querySelector(".condition1").innerHTML = data.list[4].weather[0].description;
-            document.querySelector(".condition2").innerHTML = data.list[4].weather[0].description;
-            document.querySelector(".condition3").innerHTML = data.list[4].weather[0].description;
-            document.querySelector(".condition4").innerHTML = data.list[4].weather[0].description;
-            document.querySelector(".condition5").innerHTML = data.list[4].weather[0].description;
+            document.querySelector(".condition2").innerHTML = data.list[12].weather[0].description;
+            document.querySelector(".condition3").innerHTML = data.list[22].weather[0].description;
+            document.querySelector(".condition4").innerHTML = data.list[30].weather[0].description;
+            document.querySelector(".condition5").innerHTML = data.list[37].weather[0].description;
+
+            /*for(i=0;i<39;i++){
+                document.querySelectorAll(".condition" + "i+8" + ".condition")
+            }*/
             
 
-            let weatherCondition1 = data.list[0].weather[0].main.toLowerCase();
+            let weatherCondition1 = data.list[0].weather[0].description.toLowerCase();
              document.querySelector(".weather-icon").src= `images/${weatherCondition1}.svg`;
 
-             let weatherCondition2 = data.list[4].weather[0].main.toLowerCase();
+             let weatherCondition2 = data.list[4].weather[0].description.toLowerCase();
              document.querySelector(".day1-icon").src= `images/${weatherCondition2}.svg`;
 
-             let weatherCondition3 = data.list[12].weather[0].main.toLowerCase();
+             let weatherCondition3 = data.list[12].weather[0].description.toLowerCase();
              document.querySelector(".day2-icon").src= `images/${weatherCondition3}.svg`;
 
-             let weatherCondition4 = data.list[22].weather[0].main.toLowerCase();
+             let weatherCondition4 = data.list[22].weather[0].description.toLowerCase();
              document.querySelector(".day3-icon").src= `images/${weatherCondition4}.svg`;
 
-             let weatherCondition5 = data.list[30].weather[0].main.toLowerCase();
+             let weatherCondition5 = data.list[30].weather[0].description.toLowerCase();
              document.querySelector(".day4-icon").src= `images/${weatherCondition5}.svg`;
 
-             let weatherCondition6 = data.list[37].weather[0].main.toLowerCase();
+             let weatherCondition6 = data.list[37].weather[0].description.toLowerCase();
              document.querySelector(".day5-icon").src= `images/${weatherCondition6}.svg`;
 
         
