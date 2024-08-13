baebac9a82a50d0cab2bfb22d7e3b92f25bc05b0
hellen001451 <125268394+hellen001451@users.noreply.github.com>
2024-08-13 04:30:28 +0300

    new folder

--- other.js
+++ other.js
@@ -1,4 +1,4 @@
-
+ const url = "https://api.weatherapi.com/v1/current.json?key=23acbce96342495aaf0113005242107&q=nairobi&aqi=no"
     // Get the timezone offset in seconds
     const timezoneOffset = data.city.timezone;
 
@@ -57,3 +57,61 @@
                 daysDiv.classList.add('nighttime')
             }
             }, 1000)
+
+           
+
+       // u can also use the following to get temp, weatherCondition and set weathericons if the for loops are a bit confusing//
+
+        /* document.querySelector(".temp1").innerHTML = Math.round(data.list[4].main.temp) + "°C";
+           document.querySelector(".temp2").innerHTML = Math.round(data.list[12].main.temp) + "°C";
+           document.querySelector(".temp3").innerHTML = Math.round(data.list[22].main.temp) + "°C";
+           document.querySelector(".temp4").innerHTML = Math.round(data.list[30].main.temp) + "°C";
+           document.querySelector(".temp5").innerHTML = Math.round(data.list[37].main.temp) + "°C";
+           document.querySelector(".condition1").innerHTML = data.list[4].weather[0].description;
+           document.querySelector(".condition2").innerHTML = data.list[12].weather[0].description;
+           document.querySelector(".condition3").innerHTML = data.list[22].weather[0].description;
+           document.querySelector(".condition4").innerHTML = data.list[30].weather[0].description;
+           document.querySelector(".condition5").innerHTML = data.list[37].weather[0].description;
+
+             let weatherCondition2 = data.list[4].weather[0].description.toLowerCase();
+            document.querySelector(".day1-icon").src= `images/${weatherCondition2}.svg`;
+
+            let weatherCondition3 = data.list[12].weather[0].description.toLowerCase();
+            document.querySelector(".day2-icon").src= `images/${weatherCondition3}.svg`;
+
+            let weatherCondition4 = data.list[22].weather[0].description.toLowerCase();
+            document.querySelector(".day3-icon").src= `images/${weatherCondition4}.svg`;
+
+            let weatherCondition5 = data.list[30].weather[0].description.toLowerCase();
+            document.querySelector(".day4-icon").src= `images/${weatherCondition5}.svg`;
+
+            let weatherCondition6 = data.list[37].weather[0].description.toLowerCase();
+            document.querySelector(".day5-icon").src= `images/${weatherCondition6}.svg`;
+
+           for(i=0;i<;i++){
+               document.querySelectorAll(".condition" + "i+8" + ".condition")
+           }*/
+          
+               // generate time information
+function generateTimeInformation(localTime) {
+    const date = new Date(localTime);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const time = `${hours < 10 ? `0${hours}` : hours}:${
+      minutes < 10 ? `0${minutes}` : minutes
+    } ${hours < 12 ? "AM" : "PM"}`;
+  
+    return time;
+  }
+  // update time every second
+  function updateTimeEverySecond(localTime) {
+    const date = new Date(localTime);
+    const update = () => {
+      date.setSeconds(date.getSeconds() + 1);
+      const time = generateTimeInformation(date);
+      timeInfoUI.textContent = time;
+    };
+  
+    update();
+    setInterval(update, 60000); // update every minute
+  }
--- script.js
+++ script.js
@@ -1,104 +1,120 @@
 const apiKey = "a97c5ae048f4155d874a7af8bb6486ef";
-const apiUrl ="https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
 const searchInput = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const centreDiv = document.querySelector(".centre");
 const daysDiv = document.querySelector(".days");
-        
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    
 
-async function checkWeather(city){
-    try{
-    const response= await fetch(apiUrl + city + `&appid=${apiKey}`);
+async function checkWeather(city) {
+    try {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if(response.status == 404){
-        document.querySelector(".error").style.display ="block";
-        document.querySelector(".rest").style.display ="none";
-        return;
-    }
-    const data = await response.json();
-    console.log(data);
+        if (response.status === 404) {
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".rest").style.display = "none";
+            return;
+        }
 
-     // Update the day names and weather icons for the forecast
-    for (let i = 0; i < 5; i++) {
-        const forecastIndex = i * 8; // Assuming 3-hour interval data
-        const forecastDate = new Date(data.list[forecastIndex].dt_txt);
-        const dayName = days[forecastDate.getDay()];
+        const data = await response.json();
+        console.log(data);
 
-        document.getElementById(`day${i+1}`).innerHTML = dayName;
-        document.querySelector(`.temp${i+1}`).innerHTML = Math.round(data.list[forecastIndex].main.temp) + "°C";
-        document.querySelector(`.condition${i+1}`).innerHTML = data.list[forecastIndex].weather[0].description;
-       
-        const weatherIcon = data.list[forecastIndex].weather[0].icon;
+        // Update the day names and weather icons for the forecast
+        for (let i = 0; i < 5; i++) {
+            const forecastIndex = i * 8; // Assuming 3-hour interval data
+            const forecastDate = new Date(data.list[forecastIndex].dt_txt);
+            const dayName = days[forecastDate.getDay()];
+            //const celsiusTemperature = Math.round(data.list[0].main.temp);
 
-       // Set the source for the weather icon image
-        document.querySelector(`.day${i+1}-icon`).src = `https://openweathermap.org/img/wn/${weatherIcon}.png`
+            document.getElementById(`day${i + 1}`).innerHTML = dayName;
+            document.querySelector(`.temp${i + 1}`).innerHTML = Math.round(data.list[forecastIndex].main.temp) + "°C";
+            document.querySelector(`.condition${i + 1}`).innerHTML = data.list[forecastIndex].weather[0].description;
+
+            const weatherIcon = data.list[forecastIndex].weather[0].description.toLowerCase();
+            document.querySelector(`.day${i + 1}-icon`).src = `images/${weatherIcon}.svg`;
+
+           // document.querySelector(`.day${i + 1}-icon`).src = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
+
+             //const weatherCondition1 = data.list[0].weather[0].icon;
+           // document.querySelector(".weather-icon").src = `https://openweathermap.org/img/wn/${weatherCondition1}.png`;
+        }
+
+        const weatherCondition1 = data.list[0].weather[0].description.toLowerCase();
+        document.querySelector(".weather-icon").src = `images/${weatherCondition1}.svg`;
         
+
+        document.querySelector(".city-name").innerHTML = data.city.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.list[0].main.temp);
+        document.querySelector(".wind").innerHTML = data.list[0].wind.speed + "km/hr";
+        document.querySelector(".humidity").innerHTML = data.list[0].main.humidity + "%";
+        document.querySelector(".feels_like").innerHTML = data.list[0].main.feels_like + "°C";
+        document.getElementById("description").innerHTML = data.list[0].weather[0].description;
+
+        setTemperatureConversionListeners(Math.round(data.list[0].main.temp));
+
+        setInterval(() => {
+            updateTimeAndDate();
+        }, 1000);
+
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".rest").style.display = "block";
+        searchInput.value = "";
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".rest").style.display = "none";
     }
-    let weatherCondition1 = data.list[0].weather[0].description.toLowerCase();
-             document.querySelector(".weather-icon").src= `images/${weatherCondition1}.svg`;
+}
 
+function setTemperatureConversionListeners(celsiusTemperature) {
+    const fahrenheitLink = document.querySelector("#fahrenheit-link");
+    const celsiusLink = document.querySelector("#celsius-link");
 
+    fahrenheitLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        celsiusLink.classList.remove("active");
+        fahrenheitLink.classList.add("active");
+        const temperatureElement = document.querySelector(".temp");
+        const fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+        temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+    });
 
- document.querySelector(".city-name").innerHTML = data.city.name;
- document.querySelector(".temp").innerHTML = Math.round(data.list[0].main.temp) + "°C";
- document.querySelector(".wind").innerHTML = data.list[0].wind.speed + "km/hr";
- document.querySelector(".humidity").innerHTML = data.list[0].main.humidity + "%";
- document.querySelector(".feels_like").innerHTML = data.list[0].main.feels_like + "°C";
- document.getElementById("description").innerHTML = data.list[0].weather[0].description;
+    celsiusLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        celsiusLink.classList.add("active");
+        fahrenheitLink.classList.remove("active");
+        const temperatureElement = document.querySelector(".temp");
+        temperatureElement.innerHTML = celsiusTemperature ;
+    });
+}
 
-
-
-
- setInterval(() => {
+function updateTimeAndDate() {
     const time = new Date();
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const hoursIn12HrFormat = hour % 12 || 12; // Handle 0 as 12 for 12-hour format
     const minutes = time.getMinutes();
-    const ampm = hour >=12 ? 'PM' : 'AM'
+    const ampm = hour >= 12 ? 'PM' : 'AM';
 
-     document.getElementById("time").innerHTML = (hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + ampm;
+    document.getElementById("time").innerHTML = (hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + ampm;
+    document.getElementById("date").innerHTML = days[day] + ', ' + date + ' ' + months[month];
 
-     document.getElementById("date").innerHTML = days[day] + ', ' + date + ' ' + months[month];
-
-        // Change background based on time
-        // checks if the current hour (hour) is between 6 AM (hour >= 6) and 6 PM (hour < 18)
-        if (hour >= 6 && hour < 18) {
-            centreDiv.classList.add('daytime');
-            centreDiv.classList.remove('nighttime');
-            daysDiv.classList.remove('nighttime')
-        } else {
-            centreDiv.classList.add('nighttime');
-            centreDiv.classList.remove('daytime');
-            daysDiv.classList.add('nighttime')
-        }
-        }, 1000)
-
-
-    searchInput.value = "";  
-}
-catch (error) {
-    console.error("Error fetching weather data:", error);
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".rest").style.display = "none";
-}
-}
-searchBtn.addEventListener("click", () => {
-    checkWeather(searchInput.value);
-});
-
-searchInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        checkWeather(searchInput.value);
+    // Change background based on time
+    if (hour >= 6 && hour < 18) {
+        centreDiv.classList.add('daytime');
+        centreDiv.classList.remove('nighttime');
+        daysDiv.classList.remove('nighttime');
+    } else {
+        centreDiv.classList.add('nighttime');
+        centreDiv.classList.remove('daytime');
+        daysDiv.classList.add('nighttime');
     }
-});
-
-/*// Function to handle search when the Enter key is pressed or the search button is clicked
+}
+ /*// Function to handle search when the Enter key is pressed or the search button is clicked
 function handleSearch(event) {
     if (event.key === 'Enter' || event.type === 'click') {
         checkWeather(searchInput.value);
@@ -109,38 +125,17 @@ function handleSearch(event) {
 searchInput.addEventListener('keydown', handleSearch);
 searchBtn.addEventListener('click', handleSearch);*/
 
-window.onload = () => {
-     checkWeather("Nairobi");
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchInput.value);
+});
+
+searchInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        checkWeather(searchInput.value);
     }
-       
-        // u can also use the following to get temp, weatherCondition and set weathericons if the for loops are a bit confusing//
+});
 
-         /* document.querySelector(".temp1").innerHTML = Math.round(data.list[4].main.temp) + "°C";
-            document.querySelector(".temp2").innerHTML = Math.round(data.list[12].main.temp) + "°C";
-            document.querySelector(".temp3").innerHTML = Math.round(data.list[22].main.temp) + "°C";
-            document.querySelector(".temp4").innerHTML = Math.round(data.list[30].main.temp) + "°C";
-            document.querySelector(".temp5").innerHTML = Math.round(data.list[37].main.temp) + "°C";
-            document.querySelector(".condition1").innerHTML = data.list[4].weather[0].description;
-            document.querySelector(".condition2").innerHTML = data.list[12].weather[0].description;
-            document.querySelector(".condition3").innerHTML = data.list[22].weather[0].description;
-            document.querySelector(".condition4").innerHTML = data.list[30].weather[0].description;
-            document.querySelector(".condition5").innerHTML = data.list[37].weather[0].description;
-
-              let weatherCondition2 = data.list[4].weather[0].description.toLowerCase();
-             document.querySelector(".day1-icon").src= `images/${weatherCondition2}.svg`;
-
-             let weatherCondition3 = data.list[12].weather[0].description.toLowerCase();
-             document.querySelector(".day2-icon").src= `images/${weatherCondition3}.svg`;
-
-             let weatherCondition4 = data.list[22].weather[0].description.toLowerCase();
-             document.querySelector(".day3-icon").src= `images/${weatherCondition4}.svg`;
-
-             let weatherCondition5 = data.list[30].weather[0].description.toLowerCase();
-             document.querySelector(".day4-icon").src= `images/${weatherCondition5}.svg`;
-
-             let weatherCondition6 = data.list[37].weather[0].description.toLowerCase();
-             document.querySelector(".day5-icon").src= `images/${weatherCondition6}.svg`;
-
-            for(i=0;i<;i++){
-                document.querySelectorAll(".condition" + "i+8" + ".condition")
-            }*/
+window.onload = () => {
+    checkWeather("Nairobi");
+};
